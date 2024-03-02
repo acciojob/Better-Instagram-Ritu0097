0726bef1442ed446581f9e937cab54dab8fe0273
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-02 18:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
     function dragStart(event) {
         draggedElement = event.target;
     }
-
     function dragOver(event) {
         event.preventDefault();
     }
-
     function drop(event) {
         event.preventDefault();
         if (draggedElement !== event.target) {
             const tempBackgroundImage = getComputedStyle(draggedElement).backgroundImage;
             draggedElement.style.backgroundImage = getComputedStyle(event.target).backgroundImage;
             event.target.style.backgroundImage = tempBackgroundImage;
+			
+			const tempInnerHTML = draggedElement.innerHTML;
+            draggedElement.innerHTML = event.target.innerHTML;
+            event.target.innerHTML = tempInnerHTML;
         }
     }
-
     const draggableElements = document.querySelectorAll('.image');
     draggableElements.forEach(element => {
         element.addEventListener('dragstart', dragStart);
