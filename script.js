98c8d61516777fbcb84be411f0589147f656b584
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-02 16:56:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
     function drop(event) {
         event.preventDefault();
         if (draggedElement !== event.target) {
-            const tempBackground = draggedElement.style.backgroundImage;
-            draggedElement.style.backgroundImage = event.target.style.backgroundImage;
-            event.target.style.backgroundImage = tempBackground;
+            const tempInnerHTML = draggedElement.innerHTML;
+            draggedElement.innerHTML = event.target.innerHTML;
+            event.target.innerHTML = tempInnerHTML;
         }
     }
     const draggableElements = document.querySelectorAll('.image');
@@ -20,10 +20,4 @@ document.addEventListener("DOMContentLoaded", function() {
         element.addEventListener('dragover', dragOver);
         element.addEventListener('drop', drop);
     });
-    document.getElementById('div1').style.backgroundImage = "url('https://picsum.photos/id/237/200/300')";
-    document.getElementById('div2').style.backgroundImage = "url('https://picsum.photos/seed/picsum/200/300')";
-    document.getElementById('div3').style.backgroundImage = "url('https://picsum.photos/200/300?grayscale')";
-    document.getElementById('div4').style.backgroundImage = "url('https://picsum.photos/200/300/')";
-    document.getElementById('div5').style.backgroundImage = "url('https://picsum.photos/200/300.jpg')";
-    document.getElementById('div6').style.backgroundImage = "url('https://picsum.photos/id/102/200/300')";
 });
